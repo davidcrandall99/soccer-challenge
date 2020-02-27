90d4db697d86cc62ad830954965e265e1f89358b
David Crandall <david@davidcrandall.com>
2020-02-27 14:26:20 -0800

    installing some key dependencies and jotting down outcomes

---
+++ utils/cli.js
@@ -0,0 +1,29 @@
+import arg from 'arg';
+
+function parseArgs(rawArgs) {
+	//setup some basic args to pass
+	// this will include strings of text, as well as text files
+	const args = arg(
+		{
+			'--filepath': String,
+			'--string': String,
+			'-f': '--filepath',
+			'-s': '--string',
+			'--help': Boolean,
+			'-h': '--help'
+		},
+		{
+			argv: rawArgs.slice(2),
+		}
+	);
+	return {
+		games: args['--filepath'] || args['--string'] || null
+	};
+}
+
+
+export function cli(args) {
+	let options = parseArgs(args);
+	console.log("It works:");
+	console.log(options);
+}
