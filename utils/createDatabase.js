120992cfaa5c10bfb9d689a0aae5804d0e2df06c
David Crandall <david@davidcrandall.com>
2020-02-29 10:23:10 -0800

    make a database
    moving cli.js to index, since it will have the main app logic.

--- utils/createDatabase.js
+++ utils/createDatabase.js
@@ -1,1 +1,11 @@
 // TO DO: Write code to generate a sqlite database
+const fs = require('fs');
+
+let createDatabase = () => {
+	fs.writeFileSync('./../database/matchdays.db', (data, err)=> {
+		console.log(data);
+		console.error(err);
+	});
+}
+
+module.exports = createDatabase
